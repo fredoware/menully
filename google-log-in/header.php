f0c55e8454d585a028f2e90baa93d16de323e4a5
fredowinz23 <fredowinz23@gmail.com>
2024-08-19 00:39:16 +0800

    dat

--- google-log-in/header.php
+++ google-log-in/header.php
@@ -4,7 +4,7 @@ session_regenerate_id(true);
 // change the information according to your database
 
 if ($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == '127.0.0.1') {
-  $db_connection = mysqli_connect("localhost","root","","fredoware.db");
+  $db_connection = mysqli_connect("localhost","root","","menully.db");
 } else {
   $db_connection = mysqli_connect("localhost","u437487943_fredowaresql","feZxmasqw1212!@!@","u437487943_fredowaredb");
 }
