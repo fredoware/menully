ac08fde0b0a52db6df7e20ce10a62fa87acd3e27
fredowinz23 <fredowinz23@gmail.com>
2024-10-04 16:35:27 +0800

    Added back button

--- admin/store-categories.php
+++ admin/store-categories.php
@@ -11,18 +11,18 @@ $category_list = menuCategory()->list("storeId=$Id order by priority");
 
 
 <div class="card bg-light-info shadow-none position-relative overflow-hidden">
-<div class="card-body px-4 py-3">
-    <div class="row align-items-center">
-    <div class="col-9">
-        <h4 class="fw-semibold mb-8">Categories</h4>
-    </div>
-    <div class="col-3">
-        <div class="text-center mb-n5">
-        <img src="../../dist/images/breadcrumb/ChatBc.png" alt="" class="img-fluid mb-n4">
+    <div class="card-body px-4 py-3">
+        <div class="row align-items-center">
+
+            <div class="col-2 mih-left">
+
+                <button class="btn btn-warning" onclick="history.back()"><i class="bi bi-arrow-left"></i></button>
+            </div>
+            <div class="col-9">
+                <h4 class="fw-semibold mb-8">Categories</h4>
+            </div>
         </div>
     </div>
-    </div>
-</div>
 </div>
 
 
--- admin/store-detail.php
+++ admin/store-detail.php
@@ -10,21 +10,21 @@ $peopleList = storePeople()->list("storeId=$Id order by role");
 
 
 
+
 <div class="card bg-light-info shadow-none position-relative overflow-hidden">
     <div class="card-body px-4 py-3">
         <div class="row align-items-center">
+
+            <div class="col-2 mih-left">
+
+                <button class="btn btn-warning" onclick="history.back()"><i class="bi bi-arrow-left"></i></button>
+            </div>
             <div class="col-9">
                 <h4 class="fw-semibold mb-8">Store Detail</h4>
-            </div>
-            <div class="col-3">
-                <div class="text-center mb-n5">
-                    <img src="../../dist/images/breadcrumb/ChatBc.png" alt="" class="img-fluid mb-n4">
-                </div>
             </div>
         </div>
     </div>
 </div>
-
 
 <div class="card">
     <div class="card-body">
