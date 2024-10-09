d3b79d6bf74b4b2465c03302ff405a193dda725d
fredowinz23 <fredowinz23@gmail.com>
2024-10-09 13:56:04 +0800

    Added item list

--- pages/test.php
+++ pages/test.php
@@ -45,47 +45,46 @@
     border-radius: 25px;
 }
 
-.btn-fab{
-  position: fixed;
-  top: 20px;
-  left: 30px;
-  z-index: 1000;
-  background: white;
-  border-radius: 30px;
-  padding: 15px;
-  font-size: 20px;
-  font-weight: 900;
-  border: 0px;
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2), 0 3px 10px 0 rgba(0,0,0,0.19);
+.btn-fab {
+    position: fixed;
+    top: 20px;
+    left: 30px;
+    z-index: 1000;
+    background: white;
+    border-radius: 30px;
+    padding: 15px;
+    font-size: 20px;
+    font-weight: 900;
+    border: 0px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 3px 10px 0 rgba(0, 0, 0, 0.19);
 }
 
-.cart{
-  background: green;
-  padding: 20px;
+.cart {
+    background: green;
+    padding: 20px;
 }
 
-.cart-main{
-  width: 100%;
-  position: fixed;
-  z-index: 1000;
-  bottom: 0px;
-  left: 0px;
-  margin: 0px;
+.cart-main {
+    width: 100%;
+    position: fixed;
+    z-index: 1000;
+    bottom: 0px;
+    left: 0px;
+    margin: 0px;
 }
-
 </style>
 
 
 
 <div class="row" style="background:#f0eeeb">
     <div class="col bg-white cover-size">
-      
-<button class="btn-fab" ng-click="backButton()" ng-show="btnBack"><i class="bi bi-arrow-left"></i></button>
+
+        <button class="btn-fab" ng-click="backButton()" ng-show="btnBack"><i class="bi bi-arrow-left"></i></button>
 
 
-<div class="row justify-content-center cart-main">
-<div class="col-lg-6 col-md-10 col-sm-12 cart">thi si your card</div>
-</div>
+        <div class="row justify-content-center cart-main">
+            <div class="col-lg-6 col-md-10 col-sm-12 cart">thi si your card</div>
+        </div>
         <img src="../media/<?=$store->logo?>" class="logo">
         <img src="../media/<?=$store->logo?>" class="cover-photo">
         <div class="card menu-content">
@@ -102,9 +101,16 @@
                     </div>
                 </div>
 
-                <div class="row" ng-repeat="item in itemList" ng-show="itemDisplay">
-                    <div class="col-12 mt-2">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 mt-2" ng-repeat="item in itemList" ng-show="itemDisplay">
                         <div class="card">
+
+                            <div class="card-header">
+                                <div class="square-container">
+                                    <img src="../media/{{item.image}}" width="100%">
+                                </div>
+                            </div>
+
                             <div class="card-body">
                                 {{item.name}}
 
@@ -127,7 +133,7 @@
 var app = angular.module("myApp", []);
 app.controller('myCtrl', function($scope, $http) {
     $scope.categoryList = [];
-    $scope.itemList = []; 
+    $scope.itemList = [];
     $scope.btnBack = false;
 
     $scope.getCategories = function() {
@@ -163,7 +169,7 @@ app.controller('myCtrl', function($scope, $http) {
             }
         }).then(function mySuccess(response) {
             $scope.totalRecords = response.data.total;
-            $scope.itemList= response.data.list;
+            $scope.itemList = response.data.list;
             $scope.itemDisplay = true;
             console.log("Validation", response.data)
             $scope.categoryDisplay = false;
